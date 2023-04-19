5e87c89e9ac67b43c9bf22f5587a5a654cfc0a19
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 14:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const colorSelect=document.getElementById("colorSelect");
+const removeBtn=document.querySelector('input[type="button"]');
+
+removeBtn.addEventListner("click",function()
+{
+	const selectedOpt=colorSelect.options[colorSelect.selectedIndex];
+	colorSelect.removeChild(selectOpt);
+						  });
